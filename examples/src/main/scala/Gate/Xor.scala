149b1bd1d633639776a5893dda5565d8ab7f7257
Yao Li <hnkfliyao@gmail.com>
2014-11-05 14:16:56 +0800

    Gate: Xor.

---
+++ examples/src/main/scala/Gate/Xor.scala
@@ -0,0 +1,25 @@
+package NewHDLExample.Gate.Xor
+
+import NewHDL.Core.HDLClass
+import NewHDL.Core.HDLBase._
+
+class Xor[T](clk: HDL[Boolean], rst: HDL[Boolean],
+  a: HDL[T], b: HDL[T], z: HDL[T]) extends HDLClass {
+  def xor = module {
+    sync(clk, 1) {
+      when (rst) {
+        z := 0
+      } .otherwise {
+        z := a ^ b
+      }
+    }
+  }
+
+  override val toCompile = List(xor)
+}
+
+object Main {
+  def main(args: Array[String]) {
+    println(new Xor[Boolean](0, 0, 0, 0, 0).compile)
+  }
+}
